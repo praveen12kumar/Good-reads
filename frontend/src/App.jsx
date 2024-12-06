d33b037d6546fe19c87bcf7a0baed3ac8070dbb7
Praveen Kumar <praveenshakya441@gmail.com>
2024-12-06 16:35:06 +0530

    Added absolute import configurations using vite.config and jsconfig

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
-
+import Home from '@/pages/Home';
+import Footer from '@/components/footer/Footer';
 function App() {
   
 
   return (
     <>
+    <Home/>
+    <Footer/>
     
     </>
   )
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve:{
+    alias:{
+      '@': '/src',
+      "components": ["./src/components/*"],
+      "pages": ["./src/pages/*"],
+      "assets":["./src/assets"],
+      "layouts": ["./src/layouts/*"],
+      "redux": ["./src/redux/*"],
+      "helpers":["./src/helpers/*"],
+      "routes":["./src/routes/*"],
+      "configs":["./src/configs/*"]
+    }
+  }
 })
